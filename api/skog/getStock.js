57cf22c4fcfea01dbf426736e02b9f60c8150ff8
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-18 19:12:58 +0000

    add stock check

--- api/skog/getStock.js
+++ api/skog/getStock.js
@@ -1,19 +1,36 @@
-const ikeaStoreLookup = require('../../lib/ingka/storeCheck');
+const ikeaStoreLookup = require('../../lib/ingka/storeCheck'),
+    ikeaStockLookup = require('../../lib/ingka/stockCheck');
 
-module.exports = async (req, res) => { 
-    const { buCode } = req.query;
-    
+module.exports = async (req, res) => {
+    const {
+        buCode,
+        prodCode // unused unless user asks for individual product
+    } = req.query;
+
     // check if buCode
     if (!buCode) {
-        return res.status(400).json({success: false, message: 'Missing buCode'});
+        return res.status(400).json({
+            success: false,
+            message: 'Missing buCode'
+        });
     }
-    
+
     const ikeaStore = await ikeaStoreLookup(buCode);
 
     // look up stock for store
     if (ikeaStore.success) {
+        const ikeaStock = await ikeaStockLookup(buCode, prodCode || '00402813'); // 00402813 is code for big bear 
 
+        // return stock data
+        return res.status(200).json({
+            success: ikeaStore.success,
+            store: ikeaStore.store,
+            stock: ikeaStock.stock
+        });
     }
 
-    return res.status(200).json(ikeaStore);
+    return res.status(200).json({
+        "success": false,
+        "message": "Store not found"
+    });
 };
